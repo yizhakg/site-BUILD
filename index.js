ab65d95a416d9b175453c21f7f6e32b5f8c2e84f
yizhakg <yizhakg.gmail.com>
2021-01-17 23:27:17 +0200

    update

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-function toggleMenu(){
-  let toggle = document.querySelector('.toggle');
-  let hoverBar = document.querySelector('.hoverBar');
-  let mainBar = document.querySelector('.mainBar');
-  toggle.classList.toggle('active');
-  hoverBar.classList.toggle('active');
-  mainBar.classList.toggle('active');
+function toggleMenu() {
+  let toggle = document.querySelector(".toggle");
+  let hoverBar = document.querySelector(".hoverBar");
+  let mainBar = document.querySelector(".mainBar");
+  toggle.classList.toggle("active");
+  hoverBar.classList.toggle("active");
+  mainBar.classList.toggle("active");
 }
-window.addEventListener('resize',(e)=>{
-if(window.innerWidth>800){
-  let toggle = document.querySelector('.toggle');
-  let hoverBar = document.querySelector('.hoverBar');
-  let mainBar = document.querySelector('.mainBar');
-  toggle.classList.remove('active');
-  hoverBar.classList.remove('active');
-  mainBar.classList.remove('active');
-}
-})
 function updateWindowHeight() {
   document.documentElement.style.setProperty(
     "--windowHeight",
     window.innerHeight + "px"
   );
+  if (window.innerWidth > 800) {
+    let toggle = document.querySelector(".toggle");
+    let hoverBar = document.querySelector(".hoverBar");
+    let mainBar = document.querySelector(".mainBar");
+    toggle.classList.remove("active");
+    hoverBar.classList.remove("active");
+    mainBar.classList.remove("active");
+  }
 }
 window.onload = () => {
   updateWindowHeight();
